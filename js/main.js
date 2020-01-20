c0b283eabf0292d01d415c6adbcca88d59fd9e0c
Gabriel Romero <gromero@redstage.com>
2020-01-20 17:13:06 -0300

    First commit

---
+++ js/main.js
@@ -0,0 +1,90 @@
+(function ($) {
+
+    "use strict";
+    $(".carousel-inner .item:first-child").addClass("active");
+    /* Mobile menu click then remove
+    ==========================*/
+    $(".mainmenu-area #primary_menu li a").on("click", function () {
+        $(".navbar-collapse").removeClass("in");
+    });
+    /* Scroll to top
+    ===================*/
+    $.scrollUp({
+        scrollText: '<i class="lnr lnr-arrow-up"></i>',
+        easingType: 'linear',
+        scrollSpeed: 900,
+        animation: 'fade'
+    });
+    /* testimonials Slider Active
+    =============================*/
+    $('.gallery-slide').owlCarousel({
+        loop: true,
+        margin: 0,
+        responsiveClass: true,
+        nav: false,
+        autoplay: true,
+        autoplayTimeout: 4000,
+        smartSpeed: 1000,
+        navText: ['<i class="lnr lnr-chevron-left"></i>', '<i class="lnr lnr-chevron-right"></i>'],
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 2
+            },
+            1280: {
+                items: 3
+            },
+            1500: {
+                items: 4
+            }
+        }
+    });
+
+    /*-- Smoth-Scroll --*/
+    $('.mainmenu-area a[href*="#"]')
+        // Remove links that don't actually link to anything
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function (event) {
+            // On-page links
+            if (
+                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+                location.hostname == this.hostname
+            ) {
+                // Figure out element to scroll to
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                // Does a scroll target exist?
+                if (target.length) {
+                    // Only prevent default if animation is actually gonna happen
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000, function () {
+                        // Callback after animation
+                        // Must change focus!
+                        var $target = $(target);
+                        $target.focus();
+                        if ($target.is(":focus")) { // Checking if the target was focused
+                            return false;
+                        } else {
+                            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                            $target.focus(); // Set focus again
+                        };
+                    });
+                }
+            }
+        });
+    /* Preloader Js
+    ===================*/
+    $(window).on("load", function () {
+        $('.preloader').fadeOut(500);
+        /*WoW js Active
+        =================*/
+        new WOW().init({
+            mobile: false,
+        });
+    });
+})(jQuery);
